93485c7163d01534aae6c939ebe85fa9f8cb176f
githubtest-lh <112018729+githubtest-lh@users.noreply.github.com>
2022-11-30 22:34:30 +0100

    wip

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
+const BACKEND_URL = process.env.BACKEND_URL
 const config = {
 	backendUrl:
 		process.env.NODE_ENV === 'development'
 			? 'http://localhost:9000/.netlify/functions/app'
-			:  process.env.BACKEND_URL,
+			:  BACKEND_URL,
 //			: 'https://spotify-stats-server.netlify.com/.netlify/functions/app',
 	repoUrl: 'https://github.com/sorxrob/spotify-stats',
 };
