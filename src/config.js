06d2c5c9d46491fbe5e18828002956d032821812
githubtest-lh <112018729+githubtest-lh@users.noreply.github.com>
2022-11-30 22:51:12 +0100

    fix backend-url

--- src/config.js
+++ src/config.js
@@ -1,10 +1,8 @@
-//trigger redeploy
-const BACKEND_URL = process.env.BACKEND_URL
 const config = {
 	backendUrl:
 		process.env.NODE_ENV === 'development'
 			? 'http://localhost:9000/.netlify/functions/app'
-			:  BACKEND_URL,
+			:  process.env.REACT_APP_BACKEND_URL,
 //			: 'https://spotify-stats-server.netlify.com/.netlify/functions/app',
 	repoUrl: 'https://github.com/sorxrob/spotify-stats',
 };
